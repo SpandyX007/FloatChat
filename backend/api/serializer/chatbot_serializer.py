64d77ec4066398dfe698d967200aba8fd0194f32
Spandan Ray <142141608+SpandyX007@users.noreply.github.com>
2025-09-21 15:55:40 +0530

    Delete backend/api directory

--- backend/api/serializer/chatbot_serializer.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-def query_serializer(time_duration:datetime, region:str, parameters:str):
-    return ({
-        "time_duration":time_duration,
-        "region":region,
-        "parameters":parameters,
-    })
